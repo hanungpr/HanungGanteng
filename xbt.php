6335905bc47efd921eb3b8f157ce3a385f6ec1b7
hanungpr <45429539+hanungpr@users.noreply.github.com>
2019-01-15 15:21:26 +0700

    Update xbt.php

--- xbt.php
+++ xbt.php
@@ -1,20 +1,23 @@
 <?php
-// HanungPR
-function read ($length='255') 
-{ 
-   if (!isset ($GLOBALS['StdinPointer'])) 
-   { 
-      $GLOBALS['StdinPointer'] = fopen ("php://stdin","r"); 
-   } 
-   $line = fgets ($GLOBALS['StdinPointer'],$length); 
-   return trim ($line); 
-} 
+function rand2($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
 function hpr($reff){
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, 'https://app.viral-loops.com/api/v2/events');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, '{"publicToken":"RlnFOgDUuNs_mkW38dAZJLG3_Fc","params":{"event":"registration","user":{"firstname":"didi'.rand(1000000000,9999999999).'","email":"didi'.rand(1000000000,9999999999).'@gmail.com"},"referrer":{"referralCode":"'.$reff.'"},"refSource":"copy"}}');
+    curl_setopt($ch, CURLOPT_POSTFIELDS, '{"publicToken":"RlnFOgDUuNs_mkW38dAZJLG3_Fc","params":{"event":"registration","user":{"firstname":"'.rand2(15).'","email":"'.rand2(15).'@gmail.com"},"referrer":{"referralCode":"'.$reff.'"},"refSource":"copy"}}');\
     curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
     curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
     
     $headers = array();
@@ -28,19 +31,5 @@ function hpr($reff){
     $result = curl_exec($ch);
     return $result;
 }
-echo "Reff : ";
-$reff = read();
-echo "Berapa Banyak? : ";
-$banyak = read();
-echo "\n============== START Tuyul ==============\n";
-$x = 1;
-while($x <= $banyak) {
-$ekse = json_decode(hpr($reff));
-if ($ekse->isNew == "1"){
-    echo "".$x.". SUKSES Ngereff";
-} else {
-    echo "".$x.". GAGAL Ngereff";
-}echo PHP_EOL;
-$x++;
-}
-echo "================= Done ==================\n";
+$reff = "RCb0657";
+echo hpr($reff);
